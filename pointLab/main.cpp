f763ff68e1a5a04070289c9635e09ce209860294
gabiavila4 <123492198+gabiavila4@users.noreply.github.com>
2025-11-04 10:05:43 -0600

    Initial Commit

---
+++ pointLab/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pointLab
+//
+//  Created by Gabriela Avila on 11/4/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
